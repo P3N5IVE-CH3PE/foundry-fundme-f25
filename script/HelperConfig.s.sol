c51dfd6a89856d43040909ba395b057967ebc329
P3N5IVE-CH3PE <P3N5IVE_CH3PE@protonmail.com>
2025-10-15 23:58:41 -0700

    corrected error.

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -1,11 +1,9 @@
 // SPDX-License-Identifier: MIT
 
-/* 
-1. Deploy mocks when we are on a local anvil chain
-2. Keep track of contract address across different chains
-Sepolia ETH/USD
-Mainnet ETH/USD
-*/
+//1. Deploy mocks when we are on a local anvil chain
+// 2. Keep track of contract address across different chains
+// Sepolia ETH/USD
+// Mainnet ETH/USD
 
 pragma solidity ^0.8.18;
 
@@ -36,13 +34,17 @@ contract HelperConfig is Script {
 
     function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
         // price feed address
-        NetworkConfig memory sepoliaConfig = NetworkConfig({priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306});
+        NetworkConfig memory sepoliaConfig = NetworkConfig({
+            priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        });
         return sepoliaConfig;
     }
 
     function getMainnetEthConfig() public pure returns (NetworkConfig memory) {
         // price feed address
-        NetworkConfig memory ethConfig = NetworkConfig({priceFeed: 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419});
+        NetworkConfig memory ethConfig = NetworkConfig({
+            priceFeed: 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419
+        });
         return ethConfig;
     }
 
@@ -54,10 +56,15 @@ contract HelperConfig is Script {
         // 1. Deploy the mocks
         // 2. Return the mock address
         vm.startBroadcast();
-        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(DECIMALS, INITIAL_PRICE);
+        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(
+            DECIMALS,
+            INITIAL_PRICE
+        );
         vm.stopBroadcast();
 
-        NetworkConfig memory anvilConfig = NetworkConfig({priceFeed: address(mockPriceFeed)});
+        NetworkConfig memory anvilConfig = NetworkConfig({
+            priceFeed: address(mockPriceFeed)
+        });
         return anvilConfig;
     }
 }
